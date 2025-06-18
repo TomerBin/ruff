f32d363b4535fc38845cfd4bcf8beba376a6fb29
Tomer Bin <tomerbin98@gmail.com>
2025-06-19 02:27:09 +0300

    WIP

---
+++ two_if.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+if True and False:
+    ...
+if False and True:
+    ...
---
+++ two_if_expr.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+x, y = 0, 0
+None if x else None
+None if x and y else None
+None if x and y else None
